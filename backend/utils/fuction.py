eac348365fae94234d23bdc58657218e95deb409
Mirestrepot <92109447+Mirestrepot@users.noreply.github.com>
2023-02-24 16:22:58 -0500

    Update routers Show, Delete and structure

--- backend/utils/fuction.py
+++ backend/utils/fuction.py
@@ -31,19 +31,7 @@ def find_all_users():
         raise HTTPException(status_code=404, detail="Users not found")
     
 def delete_one_user(field: str,key):
-    try:
-        user_response =  db_client.users.find_one({field: key})
-        if user_response is None:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                        detail='User not found')
-        if user_response != field:
-            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-                            detail='You are not allowed to perform this action')
-        #Delete user
-        user_response =  db_client.users.delete_one_user({field: key})
-    except:
-        raise HTTPException(status_code=404, detail="Invalid ID")  
-    
+    pass
        
         
     
@@ -51,8 +39,16 @@ def delete_one_user(field: str,key):
 def create_user(id):
     try:
         new_user = user_schema(db_client.users.find_one({"_id": id}))
+        return new_user
+    
         
 
     except:
         raise HTTPException(status_code=404, detail="Cannot create user")
     
+
+
+
+        # if user_response != key:
+        #     raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+        #                     detail='You are not allowed to perform this action')
